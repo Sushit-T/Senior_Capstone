87262d6ab51b5adaa7b1f5f844273c5df96d3c4f
SeanF2828 <follos@mail.gvsu.edu>
2024-08-12 13:25:45 -0400

    circular buffer

--- GUI_Master_Demo.py
+++ GUI_Master_Demo.py
@@ -1828,7 +1828,7 @@ class GraphGUI:
     """
     Function to initialize the data arrays and the graphical display.
     """
-    def __init__(self, root, meas_gui, max_data_points=65535):
+    def __init__(self, root, meas_gui, max_data_points=10000):
         """
         This initializes the graph widget for the three different processes.
         
@@ -1840,14 +1840,14 @@ class GraphGUI:
         self.meas_gui = meas_gui
 
         # Initialize cache file paths for different processes
-        self.cache_files = {
-            'tunneling_approach': "tunneling_approach_cache.csv",
-            'cap_approach': "cap_approach_cache.csv",
-            'enable_periodics': "enable_periodics_cache.csv",
-            'feedback_control': "feedback_control_cache.csv"
-        }
+        # self.cache_files = {
+        #     'tunneling_approach': "tunneling_approach_cache.csv",
+        #     'cap_approach': "cap_approach_cache.csv",
+        #     'enable_periodics': "enable_periodics_cache.csv",
+        #     'feedback_control': "feedback_control_cache.csv"
+        # }
         
-        self.init_cache_file()
+        # self.init_cache_file()
         
         # Configures plot
         self.fig, self.ax = plt.subplots()
@@ -1856,39 +1856,40 @@ class GraphGUI:
        
         # Initializes graphical data
         self.max_data_points = max_data_points
-        self.y_data = deque(maxlen=max_data_points)
-        self.x_data = deque(maxlen=max_data_points)
-        self.time_data = deque(maxlen=max_data_points)
+        self.y_data = [0.0 for _ in range(max_data_points)]
+        self.x_data = [None] * self.max_data_points
+        self.time_data = [None] * self.max_data_points
         self.line, = self.ax.plot([], [], 'r-')
 
         # Create a canvas to embed the figure in Tkinter
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas.get_tk_widget().grid(row=0, column=3, columnspan=6, rowspan=10, padx=10, pady=5, sticky="n")
         
-    def init_cache_file(self):
-        """
-        Initializes the cache file for storing discarded graph data.
-        """
-        headers = ["Time (s)", "Current (nA)"]
-        for _, path in self.cache_files.items():
-            with open(path, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(headers)
-                
-    def write_to_cache(self, process, x_values, y_values):
-        """
-        Writes a single data point to the cache file.
+    #def init_cache_file(self):
+    #    """
+    #    Initializes the cache file for storing discarded graph data.
+    #    """
+    #    headers = ["Time (s)", "Current (nA)"]
+    #    for _, path in self.cache_files.items():
+    #        with open(path, 'w', newline='') as file:
+    #            writer = csv.writer(file)
+    #            writer.writerow(headers)
+    #            
+    #def write_to_cache(self, process, x_values, y_values):
+    #    """
+    #    Writes a single data point to the cache file.
+    #
+    #    Args:
+    #        x_values (_type_): _description_
+    #        y_values (_type_): _description_
+    #    """
+    #    cache_file = self.cache_files.get(process)
+    #    if cache_file:
+    #        with open(cache_file, 'a', newline='') as file:
+    #            writer = csv.writer(file)
+    #            writer.writerow([x_values, y_values])        
 
-        Args:
-            x_values (_type_): _description_
-            y_values (_type_): _description_
-        """
-        cache_file = self.cache_files.get(process)
-        if cache_file:
-            with open(cache_file, 'a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([x_values, y_values])
-            
+
     def update_graph(self, process):
         """
         This will update the visual graph with the data points obtained during
@@ -1902,21 +1903,25 @@ class GraphGUI:
         global TUNN_APPR_FLAG
         global TUNN_APPROACH_ESCAPE_FLG
         global FEEDBACK_CTRL_FLAG
-        
+        global graph_index
         rollover_time = globals.ROLLOVER_GRAPH_TIME
 
         # Update data with next data points
-        self.y_data.append(curr_data)
+        self.y_data[graph_index] = curr_data
         time_now = datetime.datetime.now()
         # Append current time to x axis on graph
-        self.x_data.append(time_now)
+        self.x_data[graph_index] = time_now
         
         # Append time to include milliseconds for exported data
         formatted_time = time_now.strftime('%H:%M:%S.%f')[:-3]
-        self.time_data.append(formatted_time)
+        self.time_data[graph_index] = formatted_time
+
+        # Update graph_index
+        graph_index = ((graph_index + 1) % self.max_data_points)
+
         
         # Write every data point to the cache file for the specified process
-        self.write_to_cache(process, formatted_time, curr_data)
+        #self.write_to_cache(process, formatted_time, curr_data)
 
         # Set x-axis parameters
         self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
@@ -1943,7 +1948,7 @@ class GraphGUI:
                 update_interval = 1
                 self.line.set_data(self.x_data, self.y_data)
                 #TUNN_APPROACH_ESCAPE_FLG = 0
-            if (len(self.y_data) % update_interval == 0) and not TUNN_APPROACH_ESCAPE_FLG: # Calculate the average of y_data
+            if (graph_index % update_interval == 0) and not TUNN_APPROACH_ESCAPE_FLG: # Calculate the average of y_data
                 # self.avg_y = sum(self.y_data) / len(self.y_data) if len(self.y_data) > 0 else 0
                 # Create a constant y-value list with the average value
                 # self.avg_y_data = [self.avg_y] * len(self.x_data)
@@ -1958,7 +1963,7 @@ class GraphGUI:
         time_interval = datetime.timedelta(seconds=30)
         min_time = datetime.datetime.now() - time_interval
             
-        if len(self.y_data) % update_interval == 0:
+        if graph_index % update_interval == 0:
             if not TUNN_APPR_FLAG:
                 self.line.set_data(self.x_data, self.y_data)
                 filtered_y_data = [y for x, y in zip(self.x_data, self.y_data) if x >= min_time]
@@ -1991,15 +1996,16 @@ class GraphGUI:
         """
         Resets the visual graph and clears the data points.
         """
+        global graph_index
         self.ax.clear()
         self.ax.set_xlabel('Time (s)')
         self.ax.set_ylabel('Current (nA)')
-        self.y_data = deque(maxlen=self.max_data_points)
-        self.x_data = deque(maxlen=self.max_data_points)
-        self.time_data = deque(maxlen=self.max_data_points)
+        self.y_data = [0.0 for _ in range(self.max_data_points)]
+        self.x_data = [datetime.datetime.now() for _ in range(self.max_data_points)]
         self.line, = self.ax.plot([], [], 'r-')
         self.canvas.draw()
         self.canvas.flush_events()
+        graph_index = 0
 
 
 if __name__ == "__main__":
